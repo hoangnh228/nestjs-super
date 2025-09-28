2c5789be70d837103c1ce339080571ae4c5bf1f1
Hoang Nguyen <hoang.nguyen@twenty-tech.com>
2025-09-28 23:44:21 +0700

    add react emails

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common'
 import { Resend } from 'resend'
 import env from 'src/shared/config'
-import fs from 'fs'
-import path from 'path'
+import OtpEmail from 'emails/otp'
 
-const template = fs.readFileSync(path.resolve('src/shared/email-templates/otp.html'), 'utf8')
+// const template = fs.readFileSync(path.resolve('src/shared/email-templates/otp.html'), 'utf8')
 @Injectable()
 export class EmailService {
   private readonly resend: Resend
@@ -13,12 +12,12 @@ export class EmailService {
   }
 
   sendOtp(payload: { email: string; code: string }) {
-    console.log('here')
     return this.resend.emails.send({
       from: 'onboarding@resend.dev',
       to: 'hoangnh228@gmail.com',
       subject: 'Your OTP code',
-      html: template.replaceAll('{{code}}', payload.code).replaceAll('{{subject}}', 'Your OTP code'),
+      react: OtpEmail({ subject: 'Your OTP code', verificationCode: payload.code }),
+      // html: template.replaceAll('{{code}}', payload.code).replaceAll('{{subject}}', 'Your OTP code'),
     })
   }
 }
