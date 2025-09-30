a4eafae733d738124b087de443e6761cbba5b509
Hoang Nguyen <hoang.nguyen@twenty-tech.com>
2025-10-01 00:15:42 +0700

    refactor AuthenticationGuard

--- src/shared/guards/authentication.guard.ts
+++ src/shared/guards/authentication.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common'
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, HttpException } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { AuthTypes, ConditionGuard } from 'src/shared/constants/auth.constant'
 import { AUTH_TYPE_KEY, AuthTypeDecoratorPayload } from 'src/shared/decorators/auth.decorator'
@@ -22,40 +22,55 @@ export class AuthenticationGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const authTypeValue = this.reflector.getAllAndOverride<AuthTypeDecoratorPayload | undefined>(AUTH_TYPE_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]) || { authTypes: [AuthTypes.Bearer], options: { condition: ConditionGuard.And } }
-
+    const authTypeValue = this.getAuthTypeValue(context)
     const guards = authTypeValue.authTypes.map((authType) => this.authTypeGuardMap[authType])
-    let error = new UnauthorizedException()
+    return authTypeValue.options.condition === ConditionGuard.And
+      ? this.handleAndCondition(guards, context)
+      : this.handleOrCondition(guards, context)
+  }
 
-    if (authTypeValue.options.condition === ConditionGuard.Or) {
-      for (const guard of guards) {
-        const result = await Promise.resolve(guard.canActivate(context)).catch((err) => {
-          error = err
-          return false
-        })
+  private getAuthTypeValue(context: ExecutionContext): AuthTypeDecoratorPayload {
+    return (
+      this.reflector.getAllAndOverride<AuthTypeDecoratorPayload | undefined>(AUTH_TYPE_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]) || { authTypes: [AuthTypes.Bearer], options: { condition: ConditionGuard.And } }
+    )
+  }
 
-        if (result) {
+  private async handleOrCondition(uards: CanActivate[], context: ExecutionContext): Promise<boolean> {
+    let lastError: any = null
+    const guards = this.getAuthTypeValue(context).authTypes.map((authType) => this.authTypeGuardMap[authType])
+    for (const guard of guards) {
+      try {
+        if (await guard.canActivate(context)) {
           return true
         }
-      }
-
-      throw error
-    } else {
-      for (const guard of guards) {
-        const result = await Promise.resolve(guard.canActivate(context)).catch((err) => {
-          error = err
-          return false
-        })
-
-        if (!result) {
-          throw new UnauthorizedException()
-        }
+      } catch (error) {
+        lastError = error
       }
     }
 
+    if (lastError instanceof HttpException) {
+      throw lastError
+    }
+
+    throw new UnauthorizedException()
+  }
+
+  private async handleAndCondition(guards: CanActivate[], context: ExecutionContext) {
+    for (const guard of guards) {
+      try {
+        if (!(await guard.canActivate(context))) {
+          throw new UnauthorizedException()
+        }
+      } catch (error) {
+        if (error instanceof HttpException) {
+          throw error
+        }
+        throw new UnauthorizedException()
+      }
+    }
     return true
   }
 }
